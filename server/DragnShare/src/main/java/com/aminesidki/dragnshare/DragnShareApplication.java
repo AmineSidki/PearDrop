8e63c156530907c7fb5d8f023212cc86e58ffdf1
AmineSidki <amineyoutubw@gmail.com>
2025-07-03 18:49:29 +0100

    Initialized server

---
+++ server/DragnShare/src/main/java/com/aminesidki/dragnshare/DragnShareApplication.java
@@ -0,0 +1,13 @@
+package com.aminesidki.dragnshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragnShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DragnShareApplication.class, args);
+    }
+
+}
---
+++ server/DragnShare/src/test/java/com/aminesidki/dragnshare/DragnShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aminesidki.dragnshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragnShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
