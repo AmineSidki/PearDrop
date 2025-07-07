8e0bd3863abbd8bf1166ff563e7e79e123b625b3
AmineSidki <amineyoutubw@gmail.com>
2025-07-07 12:24:14 +0100

    I removed the client , for now

---
+++ server/DragnShare/src/main/java/com/aminesidki/dragnshare/util/UdpDiscoveryListener.java
@@ -0,0 +1,30 @@
+package com.aminesidki.dragnshare.util;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+@Component
+public class UdpDiscoveryListener implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        new Thread(this::startListening).start();
+    }
+
+    private void startListening(){
+        try(DatagramSocket socket = new DatagramSocket(8888)){
+            byte[] buffer = new byte[1024];
+            while(true){
+                DatagramPacket packet = new DatagramPacket(buffer , buffer.length);
+                socket.receive(packet);
+                String msg = new String(packet.getData(), 0, packet.getLength());
+                System.out.println(msg);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ server/DragnShare/src/main/java/com/aminesidki/dragnshare/util/UdpDiscoveryWriter.java
@@ -0,0 +1,45 @@
+package com.aminesidki.dragnshare.util;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class UdpDiscoveryWriter implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        new Thread(this::broadcast).start();
+    }
+
+    private String getLocalIP(){
+        try(DatagramSocket socket = new DatagramSocket()){
+            socket.connect(InetAddress.getByName("8.8.8.8"),10002);
+            return socket.getLocalAddress().getHostAddress();
+        }catch(Exception e){
+            return "127.0.0.1";
+        }
+    }
+
+    private void broadcast(){
+        try(DatagramSocket socket = new DatagramSocket(8888)){
+            String msg = getLocalIP();
+            byte[] buffer = msg.getBytes(StandardCharsets.UTF_8);
+            InetAddress broadcast = InetAddress.getByName("255.255.255.255");
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length ,broadcast, 8888);
+
+            while(true){
+                socket.send(packet);
+                System.out.println("Packet sent !");
+                Thread.sleep(3000);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
