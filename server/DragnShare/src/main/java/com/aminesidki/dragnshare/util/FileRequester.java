26c6057bcf191218f201cd833fec9b42fa911ff3
AmineSidki <amineyoutubw@gmail.com>
2025-07-07 13:54:25 +0100

    this is going smooth , too smooth , this might end ~BADLY~

--- server/DragnShare/src/main/java/com/aminesidki/dragnshare/util/FileRequester.java
+++ server/DragnShare/src/main/java/com/aminesidki/dragnshare/util/FileRequester.java
@@ -1,21 +1,37 @@
 package com.aminesidki.dragnshare.util;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
-public class FileRequester {
+@Component
+public class FileRequester implements CommandLineRunner {
 
-    public void resRequester(String IP) throws Exception {
-        URL url = new URL( "http://" + IP + "/file/msg");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection() ;
-        con.setRequestMethod("GET");
+    @Override
+    public void run(String... args) throws Exception {
+        new Thread(()->resRequester("192.168.1.83:8080")).start();
+    }
 
-        Scanner scanner = new Scanner(con.getInputStream());
-        StringBuilder response = new StringBuilder();
-        while(scanner.hasNext()){
-            response.append(scanner.nextLine());
+    private void resRequester(String IP){
+        try{
+            URL url = new URL( "http://" + IP + "/file/msg");
+            System.out.println("not yet !");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection() ;
+            con.setRequestMethod("GET");
+
+            System.out.println("connected !");
+
+            Scanner scanner = new Scanner(con.getInputStream());
+            StringBuilder response = new StringBuilder();
+            while(scanner.hasNext()){
+                response.append(scanner.nextLine());
+            }
+            System.out.println(response);
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        System.out.println(response);
     }
 }
