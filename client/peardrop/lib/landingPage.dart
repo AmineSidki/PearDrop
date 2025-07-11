d362dc617f02edb1d95cc1949daa97dc60a045fb
AmineSidki <amineyoutubw@gmail.com>
2025-07-11 13:23:51 +0100

    Client : finished first page , imma do the next in a week .

---
+++ client/peardrop/lib/landingPage.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+import 'modeSwitchPage.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _landingPage(context);
+  }
+}
+
+Widget _landingPage(context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text(
+        "PearDrop",
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+      ),
+      shadowColor: Colors.black,
+      elevation: 0.0,
+      backgroundColor: Colors.white,
+    ),
+    body: Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Row(
+        children: [
+          Column(
+            children: [
+              Expanded(child: SizedBox(height: 493)),
+              Container(
+                margin: EdgeInsets.only(left: 21),
+                child: Text(
+                  "Share ,\nUnconditionally .",
+                  softWrap: true,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(),
+              alignment: Alignment.centerRight,
+              child: Image.asset("assets/images/PearImage.png"),
+            ),
+          ),
+        ],
+      ),
+    ),
+    bottomNavigationBar: Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => ModeSwitchPage()),
+          );
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Text(
+                "Start",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: "Roboto",
+                  fontSize: 30.0,
+                ),
+              ),
+              constraints: BoxConstraints(maxWidth: 343.0),
+              height: 65,
+              width: 343,
+
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 81, 169, 37),
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              margin: EdgeInsets.only(left: 12, right: 12, top: 57, bottom: 20),
+              alignment: Alignment.center,
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
---
+++ client/peardrop/lib/modeSwitchPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ModeSwitchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => _modeSwitchPage(context);
+}
+
+Widget _modeSwitchPage(context) {
+  return Scaffold();
+}
